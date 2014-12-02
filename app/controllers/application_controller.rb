ffa3eaf5c7846d7c28e514c8609e1edf08879083
Evan A <evjay.a@gmail.com>
2014-12-02 13:31:39 -0700

    changed routes.rb to goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-   def goodbye
+   def hello
     render text: "¡Hola, Mundo"
+  end
+
+  def goodbye
+    render text: "Goodbye my favorite WORLD!"
   end
 end
